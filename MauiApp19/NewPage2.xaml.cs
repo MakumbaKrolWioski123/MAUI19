d8ad43e6a93d22e75a700d1e770be34cc6126278
MakumbaKrolWioski123 <ut_4010@zsp3zamosc.pl>
2026-02-13 10:24:48 +0100

    MAUI 19.4 nie dokonczone

--- MauiApp19/NewPage2.xaml.cs
+++ MauiApp19/NewPage2.xaml.cs
@@ -6,4 +6,33 @@ public partial class NewPage2 : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	void ObliczCene_Clicked(object sender, EventArgs e)
+	{
+		var imie= Imie.Text;
+		var nazwisko = Nazwisko.Text;
+		var email = Email.Text;
+        var data = Data.Date.ToString();
+        var towarzysze = DodatkoweOsobyStepper.Value.ToString();
+        if (Firma.SelectedItem==null || RadioButtonGroup.GetSelectedValue(PakietGroup)==null)
+		{
+            DisplayAlert("B³¹d", "Proszê wype³niæ wymagane pola", "OK");
+            return;
+        }
+		else 
+		{
+			var firma = Firma.SelectedItem.ToString();
+            var pakiet = RadioButtonGroup.GetSelectedValue(PakietGroup).ToString();
+            if (string.IsNullOrWhiteSpace(imie) ||  string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(firma))
+            {
+                DisplayAlert("B³¹d", "Proszê wype³niæ wymagane pola", "OK");
+            }
+            else
+            {
+                var cena = double.Parse(pakiet) + (int.Parse(towarzysze)*100);
+
+                DisplayAlert("Sukces", $"Dane zosta³y wprowadzone poprawnie:\nImiê: {imie}\nNazwisko: {nazwisko}\nEmail: {email}\nFirma: {firma}\nData: {data}\nPakiet: {pakiet}z³\nLiczba osób towarzysz¹cych: {towarzysze}\nCena ca³kowita: {cena}z³", "OK");
+            }
+        }
+    }
 }
